23eb1935d936be6c2c80b04d96b07f8fa9ac83ba
sanjeevnode <me.sanjeevks@gmail.com>
2025-06-09 23:35:53 +0530

    add service registry configuration and Docker support

--- food-service/src/main/java/com/sanjeevnode/rms/foodservice/client/AuthServiceClient.java
+++ food-service/src/main/java/com/sanjeevnode/rms/foodservice/client/AuthServiceClient.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name="auth-service", url="${auth.service.url}")
+@FeignClient(name="AUTH-SERVICE")
 public interface AuthServiceClient {
 
     @PostMapping("/auth/validate")
